602a7da5cc9e9b5e14f117c769ac8943c798d10d
Tanner Smith <tanner@tssoftware.net>
2012-12-10 18:16:30 -0500

    Close config screen and tell the widget we closed.

--- src/com/antarcticapps/tiles/AppWidgetConfigure.java
+++ src/com/antarcticapps/tiles/AppWidgetConfigure.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AppWidgetConfigure extends Activity {
 	private int mAppWidgetId;
@@ -19,11 +22,21 @@ public class AppWidgetConfigure extends Activity {
 		if (extras != null) {
 		    mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
 		}
+        
+        Button cancelButton = (Button) findViewById(R.id.cancel);
+        cancelButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+		        Intent resultValue = new Intent();
+		        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+		        setResult(RESULT_CANCELED, resultValue);
+		        finish();
+			}
+        });
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return false;
 	}
-
 }
